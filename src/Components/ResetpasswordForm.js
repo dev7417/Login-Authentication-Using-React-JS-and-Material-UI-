fb514a7b9775d22df477d7a99f47bd2797ee4bfd
Dev G <gangwardev50@gmail.com>
2022-09-19 12:36:54 +0530

    reset Password Form Validation

--- src/Components/ResetpasswordForm.js
+++ src/Components/ResetpasswordForm.js
@@ -1,8 +1,12 @@
-import React from 'react'
-import { Form, Box, Typography, Grid, TextField, Button } from '@mui/material' 
+import React, {useState} from 'react'
+import { Form, Box, Typography, Grid, TextField, Button, Alert } from '@mui/material' 
 
 export default function ResetpasswordForm() {
-
+    const[error, setError] = useState({
+        status: false,
+        msg:"",
+        type:""
+    })
     const ResetOnClick=(e)=>{
         e.preventDefault();
         const resetData = new FormData(e.currentTarget);
@@ -10,7 +14,12 @@ export default function ResetpasswordForm() {
             newpassword: resetData.get('newpassword'),
             confirmpassword: resetData.get('confirmpassword')
         }
-        console.log(getData);
+        if(getData.newpassword && getData.confirmpassword){
+            setError({status:true, msg:"Data Sumbitted Successfully", type:"success"})
+            console.log(getData);
+        }else{
+            setError({status:true, msg:"Please Filled the required fields", type:"error"});
+        }
     }
   return (
     <>
@@ -22,6 +31,7 @@ export default function ResetpasswordForm() {
             <Box sx={{mt:2}} textAlign='center'>
                 <Button  variant='contained' sm={{mt:3, mb:2, px:2}} type='submit'>Submit</Button>
             </Box>
+            <Alert severity={error.type}>{error.msg}</Alert>
            </Box>
         </Grid>
     </Grid>
